63174c7a5cf27063ac87d9d4e956fc937623cdc0
April.Chen <forchpeng@163.com>
2019-09-25 11:00:21 +0800

    Initial commit

---
+++ mall-admin/src/main/java/com/chanpion/mall/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.mall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
